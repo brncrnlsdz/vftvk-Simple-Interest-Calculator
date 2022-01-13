5a90565b1c4eaa657ee3768cedb3226ac77795eb
bruno <brunocd@tutanota.com>
2022-01-13 05:46:39 -0300

    style

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var year = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
+    var years = document.getElementById("years").value;
     var year = new Date().getFullYear()+parseInt(years);
-    result_years = new Date().getFullYear() + Number(years);
-    result_interest = Number((principal*years*rate)/100);
-    document.getElementById('result_principal').innerHTML = principal;
-    document.getElementById('result_rate').innerHTML = rate;
-    document.getElementById('result_years').innerHTML = result_years;
-    document.getElementById('result_interest').innerHTML = result_interest;
-}
-function check()
-{
-  var principal = document.getElementById('principal');
+    var interest = principal * years * rate /100;
+	
 
-    if (principal.value <= 0){
-        alert("Enter a positive number");
-        principal.focus();
-        return false;
-    }
-}
-function listen ()
+    function updateRate() 
 {
-  document.querySelector('button').addEventListener('click', () => {
-      check ();
-      compute();
-});
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
 }
-document.addEventListener('DOMContentLoaded', listen);
+ document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+   
+
+}
